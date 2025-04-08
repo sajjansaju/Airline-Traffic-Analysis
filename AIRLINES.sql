5867f236bbe28d5b35d2cb25a684dd5420351978
Nava Kumar Sajjan <137835557+sajjansaju@users.noreply.github.com>
2025-04-08 21:04:56 +1000

    Add files via upload

---
+++ AIRLINES.sql
@@ -0,0 +1,21 @@
+/*
+CREATE TABLE airlines (
+    Month TEXT,
+    In_Out CHAR(1),
+    Australian_City VARCHAR(100),
+    International_City VARCHAR(100),
+    Airline VARCHAR(100),
+    Route TEXT,
+    Port_Country VARCHAR(100),
+    Port_Region VARCHAR(50),
+    Service_Country VARCHAR(100),
+    Service_Region VARCHAR(50),
+    Stops INTEGER,
+    All_Flights INTEGER,
+    Max_Seats INTEGER,
+    Year INTEGER,
+    Month_num INTEGER
+);
+*/
+
+select * from airlines
